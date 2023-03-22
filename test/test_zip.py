f2e43465034698bde23ed6000474c060d155c62b
Dmitrii Nasedkin <2_tone@mail.ru>
2023-03-22 23:33:14 +0300

    complete tests

--- test/test_zip.py
+++ test/test_zip.py
@@ -1,70 +1,54 @@
 import os
-import csv
-import zipfile
-from zipfile import ZipFile
-from pathlib import Path
 import shutil
+import zipfile
+from pathlib import Path
+from zipfile import ZipFile
+import PyPDF2
+import pytest
 from openpyxl import load_workbook
-from PyPDF2 import PdfReader
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 destination_dir = os.path.abspath('resources')
 
-pdf_ = os.path.join(current_dir, 'resources/sample.pdf')
-xlsx_ = os.path.join(current_dir, 'resources/sample1.xlsx')
-csv_ = os.path.join(current_dir, 'resources/username.csv')
+pdf_ = os.path.abspath('sample.pdf')
+xlsx_ = os.path.abspath('sample1.xlsx')
+csv_ = os.path.abspath('username.csv')
 
 list_files = [pdf_, xlsx_, csv_]
 
 
+@pytest.fixture()
 def create_zip():
     with zipfile.ZipFile('test.zip', 'w') as zip_:
         for file in list_files:
-            zip_.write(file)
+            zip_.write(file, os.path.basename(file))
 
 
+@pytest.fixture()
 def zip_moves():
     for file in Path(current_dir).glob('test.zip'):
         shutil.move(os.path.join(current_dir, file), destination_dir)
 
 
-def test_check_pdf():
+def test_check_pdf(create_zip, zip_moves):
     with ZipFile('resources/test.zip') as zip_:
-        zip_.namelist()
-        with zip_.open('sample.pdf', 'r'):
-            reader = PdfReader('sample.pdf')
-            pdf_file = reader.pages[0].extract_text
-            assert 'A Simple PDF File' in pdf_file
+        with zip_.open('sample.pdf', 'r') as pdf_f:
+            reader = PyPDF2.PdfReader(pdf_f)
+            assert len(reader.pages) == 2
 
 
 def test_check_xlsx():
-    file_names = []
-
     with ZipFile('resources/test.zip') as zip_:
-        for file in zip_.namelist():
-            file_names.append(file)
-
-    for name in file_names:
-        if name == 'sample1.xlsx':
-            with ZipFile('resources/test.zip') as zip_:
-                with zip_.open('sample1.xlsx', 'r'):
-                    workbook = load_workbook('sample1.xlsx')
-                    sheet = workbook.active
-                    row_name = sheet.cell(2, 2).value
-                    assert row_name == 'Jane'
+        with zip_.open('sample1.xlsx', 'r'):
+            workbook = load_workbook('sample1.xlsx')
+            sheet = workbook.active
+            row_name = sheet.cell(2, 2).value
+            assert row_name == 456
 
 
 def test_check_csv():
-    file_names = []
-
     with ZipFile('resources/test.zip') as zip_:
-        for file in zip_.namelist():
-            file_names.append(file)
-
-    for name in file_names:
-        if name == 'username.csv':
-            with ZipFile('resources/test.zip') as zip_:
-                with zip_.open('username.csv', 'r') as csv_file:
-                    csv_file = csv.reader(csv_file)
-                    assert csv_file[0] == ['Username; Identifier;First name;Last name']
+        with zip_.open('username.csv', 'r') as csv_file:
+            row_ = [row for row in csv_file]
+            assert row_[0] == b'Username; Identifier;First name;Last name\n'
